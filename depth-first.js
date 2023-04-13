3c87b7b16150da2b9ce01ba1945406dc7528e1f5
John-Paul Takats <jptkts@gmail.com>
2023-04-12 21:53:08 -0400

    adding breadth

--- depth-first.js
+++ depth-first.js
@@ -1,18 +1,26 @@
+/* Without recurssion*/
+// const depthFirstPrint = (graph,source) =>{
+//     const stack = [ source ];
+
+//     while (stack.length > 0){
+//         const current = stack.pop();
+//         console.log(current);
+
+//         for (let neighbor of graph[current]){
+//             stack.push(neighbor)
+//         }
+//     }
+
+
+// };
+
+//do it with recursion
 const depthFirstPrint = (graph,source) =>{
-    const stack = [ source ];
-
-    while (stack.length > 0){
-        const current = stack.pop();
-        console.log(current);
-
-        for (let neighbor of graph[current]){
-            stack.push(neighbor)
-        }
+    console.log(source)
+    for(let neighbor of graph[source]){
+        depthFirstPrint(graph,neighbor)    
     }
-
-
-};
-
+}
 const graph = {
     a: ['b','c'],
     b: ['d'],
